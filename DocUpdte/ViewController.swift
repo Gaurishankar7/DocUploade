66eaff2e6a8fe2e340cd11d82494f5ef525c295b
Gaurishankar <gaurishankar.vibhute7@gmail.com>
2019-10-06 17:36:14 +0530

    Initial Commit

---
+++ DocUpdte/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DocUpdte
+//
+//  Created by Gaurishankar Vibhute on 06/10/19.
+//  Copyright © 2019 Gaurishankar Vibhute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: 1000)
+    }
+
+
+}
+
